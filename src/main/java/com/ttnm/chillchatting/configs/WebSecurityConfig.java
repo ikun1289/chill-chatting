6bc136893426337bfe8d00054b8203818641c9a1
ikun1289 <xhuuanng1289@gmail.com>
2022-05-13 13:21:21 +0700

    fgh

--- src/main/java/com/ttnm/chillchatting/configs/WebSecurityConfig.java
+++ src/main/java/com/ttnm/chillchatting/configs/WebSecurityConfig.java
@@ -58,7 +58,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and()
                 .csrf().disable()
                 .authorizeRequests()
-                    .antMatchers("/login","/ws/**", "/rest/message/**").permitAll() // Cho phép tất cả mọi người truy cập vào địa chỉ này
+                    .antMatchers("/login","/ws/**", "/rest/message/**","/rest/message").permitAll() // Cho phép tất cả mọi người truy cập vào địa chỉ này
                     .anyRequest().authenticated(); // Tất cả các request khác đều cần phải xác thực mới được truy cập
 
         // Thêm một lớp Filter kiểm tra jwt
